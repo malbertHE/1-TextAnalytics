92410a93cb06350abff2cd0e6737c2c15ad3a4f9
Megyesi Albert <megyesi.albert@gmail.com>
2020-03-09 23:03:43 +0100

    #14 EntropyItem tároló osztály kikényszerítése. (TDD:Piros)

---
+++ SRC/Library/MAF.Entropy.Test/EntropyItemTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MAF.Entropy.Test
+{
+    [TestClass]
+    public class EntropyItemTest
+    {
+        [TestMethod]
+        public void TestEntropyItem()
+        {
+            EntropyItem ei = new EntropyItem();
+
+        }
+    }
+}
