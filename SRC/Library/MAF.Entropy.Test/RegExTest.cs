9bfeb14d931e83bec76e38d4f910ff36c410e189
Megyesi Albert <megyesi.albert@gmail.com>
2020-03-09 20:55:24 +0100

    #9 RegEx osztály kikényszerítése. (TDD:Piros)

---
+++ SRC/Library/MAF.Entropy.Test/RegExTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MAF.Entropy.Test
+{
+    [TestClass]
+    public class RegExTest
+    {
+        [TestMethod]
+        public void TestRegEx()
+        {
+            RegEx re = new RegEx();
+        }
+    }
+}
