51d9be40e409749e9706b8cbf6e12f19e226bd9a
Megyesi Albert <megyesi.albert@gmail.com>
2020-03-09 22:13:35 +0100

    #11 EntropyCalculationLogic osztály megvalósítása. (TDD:Zöld)

---
+++ SRC/Library/MAF.Entropy/EntropyCalculationLogic.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MAF.Entropy
+{
+    /// <summary>A feldolgozás teljes logikai leírója. Csak arra használjuk, hogy az xml leírót közvetlenül 
+    /// betöltsük objektumba.</summary>
+    [Serializable]
+    public class EntropyCalculationLogic
+    {
+    }
+}
