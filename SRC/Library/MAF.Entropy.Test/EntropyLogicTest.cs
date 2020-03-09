0396377f613d6316daff6cef2f15b01d9f4591bb
Megyesi Albert <megyesi.albert@gmail.com>
2020-03-09 21:54:38 +0100

    #9 EntropyLogic osztály kikényszerítése. (TDD:Piros)

---
+++ SRC/Library/MAF.Entropy.Test/EntropyLogicTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MAF.Entropy.Test
+{
+    [TestClass]
+    public class EntropyLogicTest
+    {
+        [TestMethod]
+        public void TestEntropyLogic()
+        {
+            EntropyLogic el = new EntropyLogic();
+
+        }
+    }
+}
