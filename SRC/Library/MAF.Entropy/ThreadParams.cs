9923d3c98c58a107413758f960e06843bfa9418b
Megyesi Albert <megyesi.albert@gmail.com>
2020-03-09 23:00:07 +0100

    #13 ThreadParams tároló osztály megvalósítása. (TDD:Zöld) Belső, technikai osztály. Nem kap tesztet.

---
+++ SRC/Library/MAF.Entropy/ThreadParams.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Collections.Generic;
+
+namespace MAF.Entropy
+{
+    /// <summary>Entrópia számítás szálainak paraméterei. Technikai osztály.</summary>
+    class ThreadParams
+    {
+        public List<string> TextList = new List<string>();
+        //public List<EntropyResult> EntropyResultList = new List<EntropyResult>();
+    }
+}
