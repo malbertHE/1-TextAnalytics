d8cc5bab144a3ffdfc918163404fdf849dbe8680
Megyesi Albert <megyesi.albert@gmail.com>
2020-03-09 21:47:39 +0100

    #9 RegEx osztály Replace mezőjének megvalósítása. (TDD:Zöld)

--- SRC/Library/MAF.Entropy/RegEx.cs
+++ SRC/Library/MAF.Entropy/RegEx.cs
@@ -10,5 +10,9 @@ namespace MAF.Entropy
         /// <summary>A regex pattern része.</summary>
         [XmlAttribute(AttributeName = "Pattern")]
         public string Pattern = string.Empty;
+
+        /// <summary>A regex replace része. Ha meg van adva, akkor replace fut, ha nincs akkor csak matches.</summary>
+        [XmlAttribute(AttributeName = "Replace")]
+        public string Replace = string.Empty;
     }
 }
