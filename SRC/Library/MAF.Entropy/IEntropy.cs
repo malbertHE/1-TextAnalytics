36e104f238c152f5d02299554fb3caae049a7f4c
Megyesi Albert <megyesi.albert@gmail.com>
2020-03-09 19:47:29 +0100

    #9 IEntropy interfész megvalósítása. (TDD:Zöld)

---
+++ SRC/Library/MAF.Entropy/IEntropy.cs
@@ -0,0 +1,7 @@
+﻿namespace MAF.Entropy
+{
+    /// <summary>Entrópia számítás modul (MAF.Entropy.dll) publikált szolgáltatásait leíró interfész.</summary>
+    public interface IEntropy
+    {
+    }
+}
