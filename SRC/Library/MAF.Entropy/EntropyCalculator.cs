92f75d218059a0f1751b63b32cff180da0e63a00
Megyesi Albert <megyesi.albert@gmail.com>
2020-03-10 13:42:26 +0100

    #16 EntropyCalculator.CalculationLogic mezőjének megvalósítása. (TDD:Zöld)

--- SRC/Library/MAF.Entropy/EntropyCalculator.cs
+++ SRC/Library/MAF.Entropy/EntropyCalculator.cs
@@ -6,8 +6,14 @@ namespace MAF.Entropy
     /// </summary>
     public class EntropyCalculator
     {
+        /// <summary>Alapértelmezett logika.</summary>
+        public const string C_DefaultLogicFile = "Logic\\ecl.xml";
+
         /// <summary>A legutoljára feldolgozott forrás adat.</summary>
         public string SourceDataFile { get; private set; } = string.Empty;
+
+        /// <summary>Feldolgozási logikát leíró fájl.</summary>
+        public string CalculationLogic { get; private set; } = C_DefaultLogicFile;
 
         /// <summary>Eredményfájl. Ide kerülnek be az entrópia számítás eredményei.</summary>
         public string ResultFile { get; private set; } = string.Empty;
