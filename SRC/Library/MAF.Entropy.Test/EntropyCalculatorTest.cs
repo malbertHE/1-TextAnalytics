b9f29c47b155e35fc6a06a8273092c3a28b4215a
Megyesi Albert <megyesi.albert@gmail.com>
2020-03-10 14:24:09 +0100

    #16 EntropyCalculator.RunCalculation metódus kikényszerítése. (TDD:Piros)

--- SRC/Library/MAF.Entropy.Test/EntropyCalculatorTest.cs
+++ SRC/Library/MAF.Entropy.Test/EntropyCalculatorTest.cs
@@ -5,16 +5,25 @@ namespace MAF.Entropy.Test
     [TestClass]
     public class EntropyCalculatorTest
     {
-        [TestMethod]
-        public void TestConstructor()
+        EntropyCalculator ec;
+
+        [TestInitialize]
+        public void ClassInitialize()
         {
-            EntropyCalculator ec = new EntropyCalculator();
+            ec = new EntropyCalculator();
             Assert.IsTrue(ec.SourceDataFile == string.Empty);
             Assert.IsTrue(ec.ResultFile == string.Empty);
             Assert.IsTrue(ec.CalculationLogic == EntropyCalculator.C_DefaultLogicFile);
             Assert.IsFalse(ec.IsRunCalculation);
             Assert.IsTrue(ec.ThreadCount == 1);
             Assert.IsTrue(ec.RunningThreadCount == 0);
+        }
+
+        [TestMethod]
+        public void TestRunCalculation()
+        {
+            const string C_TestSourceFile = "test1.xml";
+            ec.RunCalculation(C_TestSourceFile);
         }
     }
 }
