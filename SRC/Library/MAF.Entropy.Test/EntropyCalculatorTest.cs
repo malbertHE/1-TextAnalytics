47e549ae023f8a959518467f1f305dc12afb9131
Megyesi Albert <megyesi.albert@gmail.com>
2020-03-10 13:02:04 +0100

    #16 EntropyCalculator osztály kikényszerítése. (TDD:Piros)

---
+++ SRC/Library/MAF.Entropy.Test/EntropyCalculatorTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MAF.Entropy.Test
+{
+    [TestClass]
+    public class EntropyCalculatorTest
+    {
+        [TestMethod]
+        public void TestEntropyCalculator()
+        {
+            EntropyCalculator ec = new EntropyCalculator();
+        }
+    }
+}
