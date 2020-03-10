c6b6e6058a6b3e41cf3abc4699b9ddcfeb330890
Megyesi Albert <megyesi.albert@gmail.com>
2020-03-10 12:32:21 +0100

    #15 EntropyResult osztály ShannonEntropy mezőjének megvalósítása. (TDD:Zöld)

--- SRC/Library/MAF.Entropy/EntropyResult.cs
+++ SRC/Library/MAF.Entropy/EntropyResult.cs
@@ -3,6 +3,9 @@
     /// <summary>Entrópia számítás eredményét leíró osztály.</summary>
     public class EntropyResult
     {
+        /// <summary>Shannon féle entrópia. H = sum(p log2 1/p)</summary>
+        public double ShannonEntropy = 0;
+
 
     }
 }
