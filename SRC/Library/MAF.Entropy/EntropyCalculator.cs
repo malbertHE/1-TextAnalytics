79ea9818dd92830b6704fd9cef1834df5303b14e
Megyesi Albert <megyesi.albert@gmail.com>
2020-03-10 13:24:47 +0100

    #16 EntropyCalculator.SourceDataFile megfelelő működésének megvalósítása. (TDD:Zöld)

--- SRC/Library/MAF.Entropy/EntropyCalculator.cs
+++ SRC/Library/MAF.Entropy/EntropyCalculator.cs
@@ -5,7 +5,14 @@
     public class EntropyCalculator
     {
         /// <summary>A forrás adat, ami feldolgozásra került.</summary>
-        public string SourceDataFile { get; private set; } = string.Empty;
+        public string SourceDataFile { get; private set; }
+
+        /// <summary>Entrópia feldolgozó osztály konstruktora.</summary>
+        /// <param name="pSourceDataFile">Forrás adat.</param>
+        public EntropyCalculator(string pSourceDataFile)
+        {
+            SourceDataFile = pSourceDataFile;
+        }
 
 
     }
