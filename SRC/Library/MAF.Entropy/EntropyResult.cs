717e26d8c1941913273d7289184bde800c0bf628
Megyesi Albert <megyesi.albert@gmail.com>
2020-03-10 12:34:51 +0100

    #15 EntropyResult osztály I mezőjének megvalósítása. (TDD:Zöld)

--- SRC/Library/MAF.Entropy/EntropyResult.cs
+++ SRC/Library/MAF.Entropy/EntropyResult.cs
@@ -6,6 +6,7 @@
         /// <summary>Shannon féle entrópia. H = sum(p log2 1/p)</summary>
         public double ShannonEntropy = 0;
 
-
+        /// <summary>Információtartalom. Az egy jelre eső átlagos információt megszorozzuk a szöveg hosszával: I = N × H</summary>
+        public double I;
     }
 }
