e23c8b0ed02df3c7d8206e8c90ed8f1425b120b1
Megyesi Albert <megyesi.albert@gmail.com>
2020-03-10 11:42:26 +0100

    #14 EntropyItem osztály Count mezőjének megvalósítása. (TDD:Zöld)

--- SRC/Library/MAF.Entropy/EntropyItem.cs
+++ SRC/Library/MAF.Entropy/EntropyItem.cs
@@ -3,6 +3,10 @@
     /// <summary>Entrópia számítás eredményének tételei.</summary>
     public class EntropyItem
     {
+        /// <summary>A jel.</summary>
+        public string Value { get; private set; } = string.Empty;
 
+        /// <summary>A jel előfordulása.</summary>
+        public uint Count;
     }
 }
