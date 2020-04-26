6abb9d24bd2989812dfab063946df9c1b630bd48
Megyesi Albert <megyesi.albert@gmail.com>
2020-04-26 12:27:14 +0200

    Konzolos alkalmazás javítása és WPF alkalmazás lefejlesztése.

---
+++ SRC/Product/MAF.Entropy.WPF/About.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace MAF.Entropy.WPF
+{
+    /// <summary>
+    /// Interaction logic for About.xaml
+    /// </summary>
+    public partial class About : Window
+    {
+        public About()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ SRC/Product/MAF.Entropy.WPF/Control/Block.xaml.cs
@@ -0,0 +1,19 @@
+﻿/*
+    Original code owner https://github.com/alandias01
+    Original code location: https://github.com/alandias01/wpfutils/blob/master/Block.xaml.cs
+*/
+using System.Windows.Controls;
+
+namespace LoadingControl.Control
+{
+    /// <summary>
+    /// Interaction logic for Block.xaml
+    /// </summary>
+    public partial class Block : UserControl
+    {
+        public Block()
+        {
+            InitializeComponent();
+        }
+    }
+}
