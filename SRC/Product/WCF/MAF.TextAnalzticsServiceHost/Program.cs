3d2c43be0ca0ec4a7c3fb926c80ee26a1c6f45b2
Megyesi Albert <megyesi.albert@gmail.com>
2020-04-29 10:51:40 +0200

    Host program lefejlesztése.

---
+++ SRC/Product/WCF/MAF.TextAnalzticsServiceHost/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ServiceModel;
+using MAF.WCFTextAnalyticsService;
+
+namespace MAF.TextAnalzticsServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host = null;
+            try
+            {
+                host = new ServiceHost(typeof(TextAnalyticsService));
+                host.Open();
+                Console.WriteLine($"Host elindítva {DateTime.Now.ToString()}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"A szerver indítása a következő hiba miatt nem lehetséges: {ex.Message}");
+                host = null;
+            }
+            Console.WriteLine("Kilépés enterrel");
+            Console.ReadLine();
+            if (host != null && host.State == CommunicationState.Opened)
+                host.Close();
+        }
+    }
+}
