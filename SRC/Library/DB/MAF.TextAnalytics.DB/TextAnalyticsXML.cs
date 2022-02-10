0a9d51bdf475c3f3641969cb612219a613c08e19
Megyesi Albert <44295612+malbertHE@users.noreply.github.com>
2022-02-10 09:10:33 +0100

    Update TextAnalyticsXML.cs

--- SRC/Library/DB/MAF.TextAnalytics.DB/TextAnalyticsXML.cs
+++ SRC/Library/DB/MAF.TextAnalytics.DB/TextAnalyticsXML.cs
@@ -1,23 +1,23 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 namespace MAF.TextAnalytics.DB.XML
 {
     public class TextAnalyticsXML
     {
         public List<XMLItem> ItemList = new List<XMLItem>();
-		public List<UserItem> UserList = new List<UserItem>();
+        public List<UserItem> UserList = new List<UserItem>();
     }
 
-	public class UserItem
-	{
-		public string LoginName;
-		public string Name;
-		public string Password;
-	}
-
-	public class XMLItem
+    public class UserItem
     {
-		public string LoginName;
+        public string LoginName;
+        public string Name;
+        public string Password;
+    }
+
+    public class XMLItem
+    {
+        public string LoginName;
         public string SourceFile;
         public string SourceFileMD5;
         public string ResultFile;
